bc90b2f4a76f91a506399055496dd99ec3a9042c
Kacper Mularczyk <kacperm4545@gmail.com>
2025-01-13 22:36:52 +0100

    Add files via upload

---
+++ rental_web/src/main/java/rental/employee/typeEdit.java
@@ -0,0 +1,67 @@
+package rental.employee;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import jakarta.ejb.EJB;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.context.Flash;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import rental.dao.TypesOfBikeDAO;
+import rental.entities.TypesOfBike;
+
+@Named
+@ViewScoped
+public class typeEdit implements Serializable {
+	private TypesOfBike typesOfBike = new TypesOfBike();
+	private TypesOfBike loaded = null;
+	
+	@Inject
+	FacesContext context;
+	
+	@Inject
+	Flash flash;
+	
+	@EJB
+	TypesOfBikeDAO typesOfBikeDAO;
+	
+	public TypesOfBike getTypesOfBike() {
+		return typesOfBike;
+	}
+	
+	public void loadData() throws IOException {
+		loaded = (TypesOfBike) flash.get("typesOfBike");
+		
+		if (loaded != null) {
+			typesOfBike = loaded;
+		} else {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błędne użycie systemu", null));
+		}
+	}
+	
+	public String saveToDb() {
+		if (loaded == null) {
+			return null; // zostan na tej samej stronie
+		}
+		
+		try {
+			if (typesOfBike.getIdType() == null) { 
+				// new record
+				typesOfBikeDAO.create(typesOfBike);
+			} else {
+				// existing record
+				typesOfBikeDAO.merge(typesOfBike);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			context.addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "wystąpił błąd podczas zapisu", null));
+			return null; // zostan na tej samej stronie
+		}
+
+		return "workerPanel?faces-redirect=true";
+	}
+}
